4305502e4212d12cd03cc3c73994170d2017aa8c
Maj Lenaršič <lenarsicmaj@gmail.com>
2016-05-08 13:21:59 +0000

    Zakljucek dela na III. nalogi pt.2

--- eracun.js
+++ eracun.js
@@ -133,6 +133,10 @@ var pesmiIzRacuna = function(racunId, callback) {
     Track.TrackId IN (SELECT InvoiceLine.TrackId FROM InvoiceLine, Invoice \
     WHERE InvoiceLine.InvoiceId = Invoice.InvoiceId AND Invoice.InvoiceId = " + racunId + ")",
     function(napaka, vrstice) {
+      // izracun davcne stopnje 
+        for (var i=0; i<vrstice.length; i++) {
+          vrstice[i].stopnja = davcnaStopnja((vrstice[i].opisArtikla.split(' (')[1]).split(')')[0], vrstice[i].zanr);
+        }
       callback( vrstice);
       //console.log(vrstice);
     })
